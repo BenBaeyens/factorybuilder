5d3ff3d69df3c816fccf89e3aafae25cfe8f964c
Kjip <kjipgamer@gmail.com>
2019-07-01 20:40:30 +0200

    Added camera speed increase
    When the selector is far away from the camera, the camera increases in speed

--- FactoryBuilder/Assets/CameraMovement.cs
+++ FactoryBuilder/Assets/CameraMovement.cs
@@ -7,18 +7,34 @@ public class CameraMovement : MonoBehaviour
 
     GameObject Selector;
 
+    [SerializeField] float maxDistance;
+    [SerializeField] float resetDistance;
     [SerializeField] Vector3 offset;
     [SerializeField] float followSpeed;
+
+    float currentSpeed;
 
     #region Methods
 
     void Start()
     {
         Selector = GameObject.Find("Selector");
+        currentSpeed = followSpeed;
     }
 
+    private void Update() {
+        Debug.Log(currentSpeed);
+        if(Vector3.Distance(transform.position, Selector.transform.position) >= maxDistance){
+            currentSpeed *= 1.05f;   
+        }else if (Vector3.Distance(transform.position, Selector.transform.position) <= resetDistance)
+        {
+            if(currentSpeed >= followSpeed * 1.05)
+            currentSpeed /= 1.02f; 
+        }
+    }
     private void LateUpdate() {
-        transform.position = Vector3.Lerp(transform.position, Selector.transform.position + offset, followSpeed * Time.deltaTime);
+        
+        transform.position = Vector3.Lerp(transform.position, Selector.transform.position + offset, currentSpeed * Time.deltaTime);
     }
 
     
