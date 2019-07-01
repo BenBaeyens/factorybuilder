f2bae2eaf65b94c797b2ba60ad6e8e5bee53a6f4
Kjip <kjipgamer@gmail.com>
2019-07-01 19:52:03 +0200

    Completed selector movement mechanics

--- FactoryBuilder/Assets/SelectorController.cs
+++ FactoryBuilder/Assets/SelectorController.cs
@@ -17,8 +17,44 @@ public class SelectorController : MonoBehaviour
         currentPos = transform.position;
     }
 
-    private void Update() {
-        if(Input.GetKeyDown(KeyCode.RightArrow)){
+    private void Update()
+    {
+        MoveRight();
+        MoveLeft();
+        MoveUp();
+        MoveDown();
+
+    }
+
+
+    private void MoveDown()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position = currentPos + new Vector3(0, 0, -jumpDistance);
+            currentPos = transform.position;
+        }
+    } 
+    private void MoveUp()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position = currentPos + new Vector3(0, 0, jumpDistance);
+            currentPos = transform.position;
+        }
+    }
+    private void MoveLeft()
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.position = currentPos + new Vector3(-jumpDistance, 0, 0);
+                currentPos = transform.position;
+            }
+        }
+    private void MoveRight()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
             transform.position = currentPos + new Vector3(jumpDistance, 0, 0);
             currentPos = transform.position;
         }
