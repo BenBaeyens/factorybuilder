1ffe17d9789c91c30a22a4ccc77b2a53334d4457
Kjip <kjipgamer@gmail.com>
2019-07-01 20:29:08 +0200

    Fixed render issues

---
+++ FactoryBuilder/Assets/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    GameObject Selector;
+
+    [SerializeField] Vector3 offset;
+    [SerializeField] float followSpeed;
+
+    #region Methods
+
+    void Start()
+    {
+        Selector = GameObject.Find("Selector");
+    }
+
+    private void LateUpdate() {
+        transform.position = Vector3.Lerp(transform.position, Selector.transform.position + offset, followSpeed * Time.deltaTime);
+    }
+
+    
+    #endregion
+}
