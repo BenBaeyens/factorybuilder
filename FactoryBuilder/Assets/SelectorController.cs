e9a2d60a4b6a3ce0fb7c1438d4f33746ffaa3504
Kjip <kjipgamer@gmail.com>
2019-07-01 19:20:28 +0200

    Added test movement controls for selector

---
+++ FactoryBuilder/Assets/SelectorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorController : MonoBehaviour
+{
+
+    [SerializeField] float jumpDistance;
+
+    Vector3 currentPos;
+
+
+    #region Methods
+
+    void Start()
+    {
+        currentPos = transform.position;
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            transform.position = currentPos + new Vector3(jumpDistance, 0, 0);
+            currentPos = transform.position;
+        }
+    }
+
+    #endregion
+}
