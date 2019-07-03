24b2db48335846704502f2968c95f36fa6b15cbc
Kjip <kjipgamer@gmail.com>
2019-07-03 11:53:57 +0200

    Added basic spitter funcionality

---
+++ FactoryBuilder/Assets/Spitter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spitter : MonoBehaviour
+{
+
+    [SerializeField] GameObject instantiateObjectTemp;
+
+    #region Methods
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 0f, 2f);
+    }
+
+    public void SpawnObject(){
+        Instantiate(instantiateObjectTemp, transform.position + Vector3.forward * 2, transform.rotation);
+    }
+
+    #endregion
+}
