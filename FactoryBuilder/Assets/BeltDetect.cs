1b61dca71945f5b3693f28871009a39a843ef766
Kjip <kjipgamer@gmail.com>
2019-07-04 21:23:23 +0200

    Added belt prefab

---
+++ FactoryBuilder/Assets/BeltDetect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltDetect : MonoBehaviour
+{
+
+
+
+    #region Methods
+
+    void Start()
+    {
+        
+    }
+
+    #endregion
+}
